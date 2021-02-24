ffc86b8c6f96fdaec6eeaa4975f7711c2b271326
zhuzheqing <575866133@qq.com>
2021-02-24 21:43:41 +0800

    ADD: 初始化前后端

---
+++ server/competition_epidemic_management_system/src/main/java/project/zzq/competition_epidemic_management_system/CompetitionEpidemicManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package project.zzq.competition_epidemic_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionEpidemicManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompetitionEpidemicManagementSystemApplication.class, args);
+    }
+
+}
---
+++ server/competition_epidemic_management_system/src/test/java/project/zzq/competition_epidemic_management_system/CompetitionEpidemicManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project.zzq.competition_epidemic_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionEpidemicManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
