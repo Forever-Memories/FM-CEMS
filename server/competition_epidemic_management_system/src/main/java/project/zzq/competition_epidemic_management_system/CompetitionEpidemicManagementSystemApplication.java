2d46a115b0debc6bd0d1200558757a88433a1a73
zhuzheqing <575866133@qq.com>
2021-03-04 16:33:39 +0800

    ADD: 引入jdbc,mysql,swagger配置

--- server/competition_epidemic_management_system/src/main/java/project/zzq/competition_epidemic_management_system/CompetitionEpidemicManagementSystemApplication.java
+++ server/competition_epidemic_management_system/src/main/java/project/zzq/competition_epidemic_management_system/CompetitionEpidemicManagementSystemApplication.java
@@ -2,8 +2,9 @@ package project.zzq.competition_epidemic_management_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CompetitionEpidemicManagementSystemApplication {
 
     public static void main(String[] args) {
